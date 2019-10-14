f8c6a165939b32700c261a04eea97d1e1d01e3c4
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2019-10-14 13:24:25 +0100

    Read Potentiometers

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 DigitalOut led1(LED1);
 DigitalOut led2(LED2,1);
 
+AnalogIn   pot1(A0);
+AnalogIn   pot2(A1);
+
+
 /* Pin Configuration for FRDM-K64F */
 C12832 lcd(D11, D13, D12, D7, D10);
 
@@ -16,6 +20,11 @@ int main() {
     while (true) {
         led1 = !led1;
         led2 = !led2;
+
+		lcd.locate(0,1);
+		lcd.printf("Pot 1 : %.2f\n", (float)pot1);
+		lcd.printf("Pot 2 : %.2f\n", (float)pot2);
+
         wait(0.5);
     }
 }
