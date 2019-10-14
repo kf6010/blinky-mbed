82f0c00a9d83c2fccd440e343d690cd38a7ec8bb
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2019-10-14 13:08:59 +0100

    LCD DIsplay in use

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include "mbed.h"
+#include "C12832.h"
+#include <stdio.h>
  
 DigitalOut led1(LED1);
 DigitalOut led2(LED2,1);
- 
+
+/* Pin Configuration for FRDM-K64F */
+C12832 lcd(D11, D13, D12, D7, D10);
+
 int main() {
+	lcd.cls();
+	lcd.locate(0,0);
+	lcd.printf("Hello worlda\n");
+
     while (true) {
         led1 = !led1;
         led2 = !led2;
