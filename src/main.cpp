9070670961d477d8284341f2185886593e881a77
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2019-10-14 12:51:22 +0100

    blink two led's

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "mbed.h"
  
 DigitalOut led1(LED1);
+DigitalOut led2(LED2);
  
 int main() {
     while (true) {
         led1 = !led1;
+        led2 = !led2;
         wait(0.5);
     }
 }
