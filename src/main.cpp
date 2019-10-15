d2f55e215bb0065a72a11e33cf7bb9cd7e01e666
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2019-10-15 20:41:23 +0100

    Variable names added

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 /* Create a Digital-output for the K64F Red led
  * The initial value defaults to 0 -- led is on
  */
-DigitalOut led1(LED_RED); 
+DigitalOut red(LED_RED); 
  
 int main() {
     while (true) {  /* while _super-loop_ */
-        led1 = !led1; /* toggle led state */
+        red = !red; /* toggle led state */
         wait(0.5);    /* pause for 0.5s */
     }
 }
